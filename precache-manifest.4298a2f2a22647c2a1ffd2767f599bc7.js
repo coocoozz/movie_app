80677261c8ec7f38fbf75d29f65d0a9cf6c97c69
kkwang <coocoozz@naver.com>
2019-11-23 20:15:46 +0900

    Updates

---
+++ precache-manifest.4298a2f2a22647c2a1ffd2767f599bc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a4188f84603e6cf4e70ee63f35b2b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e3d91c7e7593ab7e430",
+    "url": "/static/css/main.9f4069f4.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "3e3d91c7e7593ab7e430",
+    "url": "/static/js/main.dd70589c.chunk.js"
+  },
+  {
+    "revision": "3a91c9b3d362400230f7",
+    "url": "/static/js/runtime-main.0532575a.js"
+  }
+]);
